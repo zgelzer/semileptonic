a677944a56bd26fbcd110cdb407caf624b518d01
Zechariah Gelzer <zechariah-gelzer@uiowa.edu>
2015-05-21 18:02:48 -0500

    made constants more explicit

--- settings/constants.py
+++ settings/constants.py
@@ -17,47 +17,57 @@ Defines lattice QCD constants and internal tolerances.
 --------------------------------------------------------------------------------
 Definitions
 -----------
-r1 : float
-    Physical value of lattice scale r_1, defined [1] by (r_1^2 * F(r_1) = 1.0),
-    where F(r) is the static force between quarks; taken to be 0.31174 fm [2].
-    Determined by requiring continuum limit of pion decay constant f_pi at
-    physical quark masses to match that of PDG [3].
 hbarc : float
     Conversion constant (hbar * c) that equates mass or energy with inverse
-    length or inverse time; taken from PDG as 197.327 MeV fm [3].
+    length or inverse time; taken from PDG as 197.327 MeV fm [1].
+r1 : float
+    Physical value of lattice scale r_1, defined [2] by (r_1^2 * F(r_1) = 1.0),
+    where F(r) is the static force between quarks; taken to be 0.31174 fm [3].
+    Determined by requiring continuum limit of pion decay constant f_pi at
+    physical quark masses to match that of PDG [1].
 Lambda : float
     Renormalization scale characteristic of heavy-quark expansion; taken to be
     700 MeV to provide liberal widths for chiral log terms [4].
 fpi : float
     Pion decay constant f_pi that characterizes energy scale of chiral symmetry
-    breaking; taken from PDG as 130.41 MeV [3].
+    breaking; taken from PDG as 130.41 MeV [1].
+mB : float
+    Mass of B^0 meson; taken from PDG as 5279.58 MeV [1].
+mBstar : float
+    Mass of B* meson; taken from PDG as 5325.2 MeV [1].
+mBstar_s : float
+    Mass of B*_s meson; taken from PDG as 5415.4 MeV [1].
+mK : float
+    Mass of kaon; taken from PDG as 497.614 MeV [1].
+mpi : float
+    Mass of pion; taken from PDG as 134.977 MeV [1].
+Delta_B : float
+    Difference between masses of B*_s and B^0 mesons for B-->K decay, or that
+    of B* and B^0 for B-->pi decay.
+a_atol : float
+    Absolute tolerance used to deterimine equality of two lattice spacings in
+    fermi; taken to be 1e-06 fm, since difference between successive lattice
+    spacings is typically of order 1e-02 fm.
 gpi : float
     Lattice determination [5] of central value of leading order axial coupling
     constant g_pi (i.e., between pions and heavy-light mesons). Hereafter, g_pi
     is referred to simply as the heavy-light coupling constant.
-Delta_B : float
-    Difference between masses of B*_s and B^0 mesons for B-->K decay, or that
-    of B* and B^0 for B-->pi decay.
+nearzero : float
+    Number used to represent essentially zero, where exactly zero would be
+    problematic in practice; taken to be 1e-30.
 tastemults : dict of ints
     Multiplicity of tastes of staggered fermions, with tastes defined as 'P' for
     pseudoscalar, 'A' for axial vector, 'T' for tensor, 'V' for vector, and 'I'
     for singlet.
 r1_continuum : float
     Continuum value of lattice scale r_1 in lattice units (r_1 / a); taken to be
-    3.744241 [2].
-ml_continuum : float
-    Continuum value of light quark mass m_l in lattice units (a * m_l); taken to
-    be 0.0009646 [2].
+    3.744241 [3].
 mh_continuum : float
     Continuum value of heavy quark mass m_h in lattice units (a * m_h); taken to
-    be 0.02645 [2].
-a_atol : float
-    Absolute tolerance used to deterimine equality of two lattice spacings in
-    fermi; taken to be 1e-06 fm, since difference between successive lattice
-    spacings is typically of order 1e-02 fm. 
-nearzero : float
-    Number used to represent essentially zero, where exactly zero would be
-    problematic in practice; taken to be 1e-30.
+    be 0.02645 [3].
+ml_continuum : float
+    Continuum value of light quark mass m_l in lattice units (a * m_l); taken to
+    be 0.0009646 [3].
 --------------------------------------------------------------------------------
 Notes
 -----
@@ -66,12 +76,12 @@ Notes
 --------------------------------------------------------------------------------
 References
 ----------
-[1] C. Bernard, et al., "The static quark potential in three flavor QCD", Phys.
+[1] K. Olive, et al. (Particle Data Group), Chin. Phys. C, 38, 090001 (2014).
+[2] C. Bernard, et al., "The static quark potential in three flavor QCD", Phys.
     Rev. D62, 034503 (2000), [arXiv:0002028 [hep-lat]].
-[2] C. Bernard, et al. (Fermilab Lattice and MILC Collaborations),
+[3] C. Bernard, et al. (Fermilab Lattice and MILC Collaborations),
     "UPDATED mass-independent r1 values",
     <http://physics.wustl.edu/~cb/Fermilab-MILC/secure/>, accessed 2015-03-30.
-[3] K. Olive, et al. (Particle Data Group), Chin. Phys. C, 38, 090001 (2014).
 [4] A. Kronfeld and J. Simone, "Computation of Lambda-bar and lambda_1 with
     Lattice QCD", Phys. Lett. B 490, 228 (2000) [Erratum-ibid. B 495, 441
     (2000)] [arXiv:0006345 [hep-ph]].
@@ -85,19 +95,24 @@ References
 from settings.fit import decayname
 
 
-r1 = 0.31174
 hbarc = 197.327
-Lambda = 700 * r1 / hbarc
-fpi = 130.41 * r1 / hbarc
-gpi = 0.45
+r1 = 0.31174
+Lambda = 700 / hbarc * r1
+fpi = 130.41 / hbarc * r1
+mB = 5279.58 / hbarc * r1
+mBstar = 5325.2 / hbarc * r1
+mBstar_s = 5415.4 / hbarc * r1
+mK = 497.614 / hbarc * r1
+mpi = 134.977 / hbarc * r1
 if decayname == 'B2K':
-    Delta_B = (5415.4 - 5279.58) * r1 / hbarc
+    Delta_B = mBstar_s - mB
 elif decayname == 'B2pi':
-    Delta_B = (5325.2 - 5279.58) * r1 / hbarc
+    Delta_B = mBstar - mB
+a_atol = 1e-06
+gpi = 0.45
+nearzero = 1e-30
 tastemults = {'P': 1, 'A': 4, 'T': 6, 'V': 4, 'I': 1}
 r1_continuum = 3.744241
-ml_continuum = r1_continuum * 0.0009646
 mh_continuum = r1_continuum * 0.02645
-a_atol = 1e-06
-nearzero = 1e-30
+ml_continuum = r1_continuum * 0.0009646
 
